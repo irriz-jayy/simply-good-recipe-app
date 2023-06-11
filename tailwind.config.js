0e597576850a4eb87355a5aa62393703f554f976
irriz-jayy <jayfelix789@gmail.com>
2023-06-11 20:39:18 +0300

    added sidebar code

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,20 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        curve: ['"DM Serif Display"'],
+      },
+      colors: {
+        background1: "#85B7DC", //(50%)
+        background2: "#85B7DC", //30%
+        background3: "85B7DC", //70%
+        inputbackground: "#D9D9D9",
+        button: "#423EEA",
+        font1: "#000000",
+        link: "#423EEA",
+      },
+    },
   },
   plugins: [],
 };

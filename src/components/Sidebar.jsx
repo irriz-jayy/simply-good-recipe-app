3c0710764ead01b04752d4f6c893ac57cad99b10
irriz-jayy <jayfelix789@gmail.com>
2023-06-12 12:38:38 +0300

    added colors

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -6,8 +6,6 @@ import {
   CakeIcon,
 } from "@heroicons/react/24/outline";
 
-// import IoIosLogOut from "react-icons/io";
-
 const navigation = [
   { name: "Home", icon: HomeIcon, href: "/", current: true },
   { name: "Recipes", icon: CakeIcon, href: "#", current: false },
@@ -27,32 +25,33 @@ function classNames(...classes) {
 
 export default function Example() {
   return (
-    <div className="flex flex-grow flex-col overflow-y-auto border-r border-gray-200 bg-white pt-5 pb-4">
+    <div className="flex flex-grow flex-col overflow-y-auto border-r border-gray-200 bg-background1 pt-5 pb-4">
       <div className="flex flex-shrink-0 items-center space-y-5 px-4">
         <img
           className="h-8 w-auto"
-          src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+          src="https://tailwindui.com/img/logos/mark.svg?color=blue&shade=400"
           alt="Your Company"
         />
+        <p className="font-curve font-medium m-2">Simply Good</p>
       </div>
       <div className="mt-5 flex flex-grow flex-col">
-        <nav className="flex-1 space-y-1 bg-white" aria-label="Sidebar">
+        <nav className="flex-1 space-y-1 bg-background1" aria-label="Sidebar">
           {navigation.map((item) => (
             <a
               key={item.name}
               href={item.href}
               className={classNames(
                 item.current
-                  ? "bg-indigo-50 border-indigo-600 text-indigo-600"
-                  : "border-transparent text-gray-600 hover:bg-gray-50 hover:text-gray-900",
+                  ? "bg-background1 border-blue-400 text-font1"
+                  : "border-transparent text-font1 hover:bg-background2 hover:opacity-30 hover:text-font1",
                 "group flex items-center px-3 py-2 text-sm font-medium border-l-4"
               )}
             >
               <item.icon
                 className={classNames(
                   item.current
-                    ? "text-indigo-500"
-                    : "text-gray-400 group-hover:text-gray-500",
+                    ? "text-font1"
+                    : "text-font1 group-hover:text-font1",
                   "mr-3 flex-shrink-0 h-6 w-6"
                 )}
                 aria-hidden="true"

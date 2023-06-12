fbf1c960551dc2d89d7f760c66c60421bd934c6d
irriz-jayy <jayfelix789@gmail.com>
2023-06-12 15:03:38 +0300

    rendered profile to the right

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import "./App.css";
+import Profile from "./components/Profile";
 import Sidebar from "./components/Sidebar";
 
 function App() {
   return (
     <div className="App">
-      <Sidebar />
+      <div className="flex">
+        <Sidebar />
+        <Profile />
+      </div>
     </div>
   );
 }
---
+++ src/components/Profile.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+function Profile() {
+  return (
+    <main className="flex-1">
+      {" "}
+      <div className="py-6">
+        {" "}
+        <div className="mx-auto max-w-7xl px-4 sm:px-6 md:px-8">
+          {" "}
+          <h1 className="text-2xl font-semibold text-gray-900">
+            {" "}
+            Profile{" "}
+          </h1>{" "}
+        </div>{" "}
+        <div className="mx-auto max-w-7xl px-4 sm:px-6 md:px-8">
+          {" "}
+          {/* Replace with your content */}{" "}
+          <div className="py-4">
+            {" "}
+            <div className="h-96 rounded-lg border-4 border-dashed border-gray-200" />{" "}
+          </div>{" "}
+          {/* /End replace */}{" "}
+        </div>{" "}
+      </div>{" "}
+    </main>
+  );
+}
+
+export default Profile;

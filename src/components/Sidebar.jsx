ca2719134be9cc18e0020bdbb1fc4b83a764cfc3
irriz-jayy <jayfelix789@gmail.com>
2023-06-12 12:20:39 +0300

    replaced icons

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,19 +1,24 @@
 import {
-  CalendarIcon,
-  ChartBarIcon,
-  FolderIcon,
+  StarIcon,
   HomeIcon,
-  InboxIcon,
-  UsersIcon,
+  UserCircleIcon,
+  ArrowRightOnRectangleIcon,
+  CakeIcon,
 } from "@heroicons/react/24/outline";
 
+// import IoIosLogOut from "react-icons/io";
+
 const navigation = [
-  { name: "Dashboard", icon: HomeIcon, href: "#", current: true },
-  { name: "Team", icon: UsersIcon, href: "#", count: 3, current: false },
-  { name: "Projects", icon: FolderIcon, href: "#", count: 4, current: false },
-  { name: "Calendar", icon: CalendarIcon, href: "#", current: false },
-  { name: "Documents", icon: InboxIcon, href: "#", count: 12, current: false },
-  { name: "Reports", icon: ChartBarIcon, href: "#", current: false },
+  { name: "Home", icon: HomeIcon, href: "/", current: true },
+  { name: "Recipes", icon: CakeIcon, href: "#", current: false },
+  { name: "Favourites", icon: StarIcon, href: "#", current: false },
+  { name: "Profile", icon: UserCircleIcon, href: "#", current: false },
+  {
+    name: "Logout",
+    icon: ArrowRightOnRectangleIcon,
+    href: "#",
+    current: false,
+  },
 ];
 
 function classNames(...classes) {
@@ -22,76 +27,40 @@ function classNames(...classes) {
 
 export default function Example() {
   return (
-    <div className="flex min-h-0 flex-1 flex-col border-r border-gray-200 bg-white">
-      <div className="flex flex-1 flex-col overflow-y-auto pt-5 pb-4">
-        <div className="flex flex-shrink-0 items-center px-4">
-          <img
-            className="h-8 w-auto"
-            src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-            alt="Your Company"
-          />
-        </div>
-        <nav
-          className="mt-5 flex-1 space-y-1 bg-white px-2"
-          aria-label="Sidebar"
-        >
+    <div className="flex flex-grow flex-col overflow-y-auto border-r border-gray-200 bg-white pt-5 pb-4">
+      <div className="flex flex-shrink-0 items-center space-y-5 px-4">
+        <img
+          className="h-8 w-auto"
+          src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+          alt="Your Company"
+        />
+      </div>
+      <div className="mt-5 flex flex-grow flex-col">
+        <nav className="flex-1 space-y-1 bg-white" aria-label="Sidebar">
           {navigation.map((item) => (
             <a
               key={item.name}
               href={item.href}
               className={classNames(
                 item.current
-                  ? "bg-gray-100 text-gray-900 hover:text-gray-900 hover:bg-gray-100"
-                  : "text-gray-600 hover:text-gray-900 hover:bg-gray-50",
-                "group flex items-center px-2 py-2 text-sm font-medium rounded-md"
+                  ? "bg-indigo-50 border-indigo-600 text-indigo-600"
+                  : "border-transparent text-gray-600 hover:bg-gray-50 hover:text-gray-900",
+                "group flex items-center px-3 py-2 text-sm font-medium border-l-4"
               )}
             >
               <item.icon
                 className={classNames(
                   item.current
-                    ? "text-gray-500"
+                    ? "text-indigo-500"
                     : "text-gray-400 group-hover:text-gray-500",
                   "mr-3 flex-shrink-0 h-6 w-6"
                 )}
                 aria-hidden="true"
               />
-              <span className="flex-1">{item.name}</span>
-              {item.count ? (
-                <span
-                  className={classNames(
-                    item.current
-                      ? "bg-white"
-                      : "bg-gray-100 group-hover:bg-gray-200",
-                    "ml-3 inline-block py-0.5 px-3 text-xs font-medium rounded-full"
-                  )}
-                >
-                  {item.count}
-                </span>
-              ) : null}
+              {item.name}
             </a>
           ))}
         </nav>
-      </div>
-      <div className="flex flex-shrink-0 border-t border-gray-200 p-4">
-        <a href="#" className="group block w-full flex-shrink-0">
-          <div className="flex items-center">
-            <div>
-              <img
-                className="inline-block h-9 w-9 rounded-full"
-                src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                alt=""
-              />
-            </div>
-            <div className="ml-3">
-              <p className="text-sm font-medium text-gray-700 group-hover:text-gray-900">
-                Tom Cook
-              </p>
-              <p className="text-xs font-medium text-gray-500 group-hover:text-gray-700">
-                View profile
-              </p>
-            </div>
-          </div>
-        </a>
       </div>
     </div>
   );

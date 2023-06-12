a9fe3bf53d56f7e2990644083c99cfe785e35ce3
irriz-jayy <jayfelix789@gmail.com>
2023-06-12 21:52:17 +0300

    added card intended for profile

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import "./App.css";
 import Profile from "./components/Profile";
-import Sidebar from "./components/Sidebar";
+// import Sidebar from "./components/Sidebar";
 
 function App() {
   return (
     <div className="App">
-      <div className="flex">
-        <Sidebar />
-        <Profile />
-      </div>
+      {/* <Sidebar /> */}
+      <Profile />
     </div>
   );
 }
--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,29 +1,37 @@
 import React from "react";
+import Sidebar from "./Sidebar";
 
 function Profile() {
   return (
-    <main className="flex-1">
-      {" "}
-      <div className="py-6">
-        {" "}
-        <div className="mx-auto max-w-7xl px-4 sm:px-6 md:px-8">
-          {" "}
-          <h1 className="text-2xl font-semibold text-gray-900">
-            {" "}
-            Profile{" "}
-          </h1>{" "}
-        </div>{" "}
-        <div className="mx-auto max-w-7xl px-4 sm:px-6 md:px-8">
-          {" "}
-          {/* Replace with your content */}{" "}
-          <div className="py-4">
-            {" "}
-            <div className="h-96 rounded-lg border-4 border-dashed border-gray-200" />{" "}
-          </div>{" "}
-          {/* /End replace */}{" "}
-        </div>{" "}
-      </div>{" "}
-    </main>
+    <>
+      <div className="flex">
+        <Sidebar />
+        <div className="flex-1">
+          <main>
+            <div className="py-6">
+              <div className="mx-auto max-w-7xl px-4 sm:px-6 md:px-8">
+                <h1 className="text-2xl font-semibold text-gray-900">
+                  My Profile
+                </h1>
+              </div>
+              <div className="mx-auto max-w-7xl px-4 sm:px-6 md:px-8">
+                <div className="py-4">
+                  <div className="h-96 rounded-lg border-4  border-gray-200 flex">
+                    <img className="h-full w-1/4" src="" />
+                    <div className="grid grid-cols-2 border border-red-800 w-3/4">
+                      <div className="col-span-2">name</div>
+                      <div>Recipes</div>
+                      <div>stars</div>
+                    </div>
+                  </div>
+                </div>
+                {/* /End replace */}
+              </div>
+            </div>
+          </main>
+        </div>
+      </div>
+    </>
   );
 }
 

26121642fb3035da55a88dc6e19e1f269d214d1b
irriz-jayy <jayfelix789@gmail.com>
2023-06-13 21:40:19 +0300

    minor styling

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -12,41 +12,133 @@ function Homepage() {
               All recipes
             </div>
             <div className="border border-4 border-red-800 m-4 p-2 grid grid-cols-3 gap-4">
-              <div className="text-center border border-4 border-red-800 h-96">
-                card
+              <div className="text-center border border-4 border-red-800 h-96 rounded-md">
+                <img src="" alt="" className="h-3/4 p-2" />
+                <div className="grid grid-cols-2">
+                  <p className="">Name</p>
+                  <p className="">Time</p>
+                  <p className="col-span-2">Servings</p>
+                </div>
               </div>
-              <div className="text-center border border-4 border-red-800 h-96">
-                card
+              <div className="text-center border border-4 border-red-800 h-96 rounded-md">
+                <img src="" alt="" className="h-3/4 p-2" />
+                <div className="grid grid-cols-2">
+                  <p className="">Name</p>
+                  <p className="">Time</p>
+                  <p className="col-span-2">Servings</p>
+                </div>
               </div>
-              <div className="text-center border border-4 border-red-800 h-96">
-                card
+              <div className="text-center border border-4 border-red-800 h-96 rounded-md">
+                <img src="" alt="" className="h-3/4 p-2" />
+                <div className="grid grid-cols-2">
+                  <p className="">Name</p>
+                  <p className="">Time</p>
+                  <p className="col-span-2">Servings</p>
+                </div>
               </div>
-              <div className="text-center border border-4 border-red-800 h-96">
-                card
+              <div className="text-center border border-4 border-red-800 h-96 rounded-md">
+                <img src="" alt="" className="h-3/4 p-2" />
+                <div className="grid grid-cols-2">
+                  <p className="">Name</p>
+                  <p className="">Time</p>
+                  <p className="col-span-2">Servings</p>
+                </div>
               </div>
-              <div className="text-center border border-4 border-red-800 h-96">
-                card
+              <div className="text-center border border-4 border-red-800 h-96 rounded-md">
+                <img src="" alt="" className="h-3/4 p-2" />
+                <div className="grid grid-cols-2">
+                  <p className="">Name</p>
+                  <p className="">Time</p>
+                  <p className="col-span-2">Servings</p>
+                </div>
               </div>
-              <div className="text-center border border-4 border-red-800 h-96">
-                card
+              <div className="text-center border border-4 border-red-800 h-96 rounded-md">
+                <img src="" alt="" className="h-3/4 p-2" />
+                <div className="grid grid-cols-2">
+                  <p className="">Name</p>
+                  <p className="">Time</p>
+                  <p className="col-span-2">Servings</p>
+                </div>
               </div>
-              <div className="text-center border border-4 border-red-800 h-96">
-                card
+              <div className="text-center border border-4 border-red-800 h-96 rounded-md">
+                <img src="" alt="" className="h-3/4 p-2" />
+                <div className="grid grid-cols-2">
+                  <p className="">Name</p>
+                  <p className="">Time</p>
+                  <p className="col-span-2">Servings</p>
+                </div>
               </div>
-              <div className="text-center border border-4 border-red-800 h-96">
-                card
+              <div className="text-center border border-4 border-red-800 h-96 rounded-md">
+                <img src="" alt="" className="h-3/4 p-2" />
+                <div className="grid grid-cols-2">
+                  <p className="">Name</p>
+                  <p className="">Time</p>
+                  <p className="col-span-2">Servings</p>
+                </div>
               </div>
-              <div className="text-center border border-4 border-red-800 h-96">
-                card
+              <div className="text-center border border-4 border-red-800 h-96 rounded-md">
+                <img src="" alt="" className="h-3/4 p-2" />
+                <div className="grid grid-cols-2">
+                  <p className="">Name</p>
+                  <p className="">Time</p>
+                  <p className="col-span-2">Servings</p>
+                </div>
               </div>
-              <div className="text-center border border-4 border-red-800 h-96">
-                card
+              <div className="text-center border border-4 border-red-800 h-96 rounded-md">
+                <img src="" alt="" className="h-3/4 p-2" />
+                <div className="grid grid-cols-2">
+                  <p className="">Name</p>
+                  <p className="">Time</p>
+                  <p className="col-span-2">Servings</p>
+                </div>
               </div>
-              <div className="text-center border border-4 border-red-800 h-96">
-                card
+              <div className="text-center border border-4 border-red-800 h-96 rounded-md">
+                <img src="" alt="" className="h-3/4 p-2" />
+                <div className="grid grid-cols-2">
+                  <p className="">Name</p>
+                  <p className="">Time</p>
+                  <p className="col-span-2">Servings</p>
+                </div>
               </div>
-              <div className="text-center border border-4 border-red-800 h-96">
-                card
+              <div className="text-center border border-4 border-red-800 h-96 rounded-md">
+                <img src="" alt="" className="h-3/4 p-2" />
+                <div className="grid grid-cols-2">
+                  <p className="">Name</p>
+                  <p className="">Time</p>
+                  <p className="col-span-2">Servings</p>
+                </div>
+              </div>
+              <div className="text-center border border-4 border-red-800 h-96 rounded-md">
+                <img src="" alt="" className="h-3/4 p-2" />
+                <div className="grid grid-cols-2">
+                  <p className="">Name</p>
+                  <p className="">Time</p>
+                  <p className="col-span-2">Servings</p>
+                </div>
+              </div>
+              <div className="text-center border border-4 border-red-800 h-96 rounded-md">
+                <img src="" alt="" className="h-3/4 p-2" />
+                <div className="grid grid-cols-2">
+                  <p className="">Name</p>
+                  <p className="">Time</p>
+                  <p className="col-span-2">Servings</p>
+                </div>
+              </div>
+              <div className="text-center border border-4 border-red-800 h-96 rounded-md">
+                <img src="" alt="" className="h-3/4 p-2" />
+                <div className="grid grid-cols-2">
+                  <p className="">Name</p>
+                  <p className="">Time</p>
+                  <p className="col-span-2">Servings</p>
+                </div>
+              </div>
+              <div className="text-center border border-4 border-red-800 h-96 rounded-md">
+                <img src="" alt="" className="h-3/4 p-2" />
+                <div className="grid grid-cols-2">
+                  <p className="">Name</p>
+                  <p className="">Time</p>
+                  <p className="col-span-2">Servings</p>
+                </div>
               </div>
             </div>
           </main>

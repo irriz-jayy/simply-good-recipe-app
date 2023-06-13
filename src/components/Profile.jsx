b88ddc11d4fa8ad6904f3313a6d7e6328c453787
irriz-jayy <jayfelix789@gmail.com>
2023-06-13 10:24:22 +0300

    added image

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -17,7 +17,12 @@ function Profile() {
               <div className="mx-auto max-w-7xl px-4 sm:px-6 md:px-8">
                 <div className="py-4">
                   <div className="h-96 rounded-lg border-4  border-gray-200 flex">
-                    <img className="h-full w-1/4" src="" />
+                    <div className="flex items-center justify-center">
+                      <img
+                        className="h-80 w-80"
+                        src="https://cdn.britannica.com/86/145786-050-5BD27317/chef-cooking-restaurant-kitchen.jpg"
+                      />
+                    </div>
                     <div className="grid grid-cols-2 border border-red-800 w-3/4">
                       <div className="col-span-2">name</div>
                       <div>Recipes</div>

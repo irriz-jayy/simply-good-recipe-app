477407a9e9e928fa27bc54333a32397c8e2a7a6c
irriz-jayy <jayfelix789@gmail.com>
2023-06-13 21:01:53 +0300

    added initial cards

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -4,8 +4,53 @@ import Sidebar from "./Sidebar";
 function Homepage() {
   return (
     <>
-      <div className="flex-1 bg-background2 bg-opacity-30">
+      <div className="flex">
         <Sidebar />
+        <div className="flex-1 bg-background2 bg-opacity-30">
+          <main>
+            <div className="text-center font-medium font-curve text-4xl ">
+              All recipes
+            </div>
+            <div className="border border-4 border-red-800 m-4 p-2 grid grid-cols-3 gap-4">
+              <div className="text-center border border-4 border-red-800 h-96">
+                card
+              </div>
+              <div className="text-center border border-4 border-red-800 h-96">
+                card
+              </div>
+              <div className="text-center border border-4 border-red-800 h-96">
+                card
+              </div>
+              <div className="text-center border border-4 border-red-800 h-96">
+                card
+              </div>
+              <div className="text-center border border-4 border-red-800 h-96">
+                card
+              </div>
+              <div className="text-center border border-4 border-red-800 h-96">
+                card
+              </div>
+              <div className="text-center border border-4 border-red-800 h-96">
+                card
+              </div>
+              <div className="text-center border border-4 border-red-800 h-96">
+                card
+              </div>
+              <div className="text-center border border-4 border-red-800 h-96">
+                card
+              </div>
+              <div className="text-center border border-4 border-red-800 h-96">
+                card
+              </div>
+              <div className="text-center border border-4 border-red-800 h-96">
+                card
+              </div>
+              <div className="text-center border border-4 border-red-800 h-96">
+                card
+              </div>
+            </div>
+          </main>
+        </div>
       </div>
     </>
   );

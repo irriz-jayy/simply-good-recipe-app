f47c01af490168e533256db74cd56b7559890809
irriz-jayy <jayfelix789@gmail.com>
2023-06-13 14:22:42 +0300

    added homepage routing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import Profile from "./components/Profile";
+import Homepage from "./components/Homepage";
 
 function App() {
   return (
     <div className="App">
       <Routes>
+        <Route path="/" element={<Homepage />} />
         <Route path="/my-profile" element={<Profile />} />
       </Routes>
     </div>

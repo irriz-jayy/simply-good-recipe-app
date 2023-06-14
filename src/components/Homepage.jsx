35c1303c8cb809997e65225dee94fe86c584f8bb
irriz-jayy <jayfelix789@gmail.com>
2023-06-14 11:00:51 +0300

    changed card styling to accomodate hover to display name

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -1,7 +1,25 @@
 import React from "react";
 import Sidebar from "./Sidebar";
+import { useState } from "react";
 
 function Homepage() {
+  const [isHovered, setIsHovered] = useState(false);
+
+  const handleMouseEnter = () => {
+    setIsHovered(true);
+  };
+
+  const handleMouseLeave = () => {
+    setIsHovered(false);
+  };
+
+  const handleTouchStart = () => {
+    setIsHovered(true);
+  };
+
+  const handleTouchEnd = () => {
+    setIsHovered(false);
+  };
   return (
     <>
       <div className="flex">
@@ -20,125 +38,27 @@ function Homepage() {
                   <p className="col-span-2">Servings</p>
                 </div>
               </div>
-              <div className="text-center border border-4 border-red-800 h-96 rounded-md">
-                <img src="" alt="" className="h-3/4 p-2" />
-                <div className="grid grid-cols-2">
-                  <p className="">Name</p>
-                  <p className="">Time</p>
-                  <p className="col-span-2">Servings</p>
-                </div>
-              </div>
-              <div className="text-center border border-4 border-red-800 h-96 rounded-md">
-                <img src="" alt="" className="h-3/4 p-2" />
-                <div className="grid grid-cols-2">
-                  <p className="">Name</p>
-                  <p className="">Time</p>
-                  <p className="col-span-2">Servings</p>
-                </div>
-              </div>
-              <div className="text-center border border-4 border-red-800 h-96 rounded-md">
-                <img src="" alt="" className="h-3/4 p-2" />
-                <div className="grid grid-cols-2">
-                  <p className="">Name</p>
-                  <p className="">Time</p>
-                  <p className="col-span-2">Servings</p>
-                </div>
-              </div>
-              <div className="text-center border border-4 border-red-800 h-96 rounded-md">
-                <img src="" alt="" className="h-3/4 p-2" />
-                <div className="grid grid-cols-2">
-                  <p className="">Name</p>
-                  <p className="">Time</p>
-                  <p className="col-span-2">Servings</p>
-                </div>
-              </div>
-              <div className="text-center border border-4 border-red-800 h-96 rounded-md">
-                <img src="" alt="" className="h-3/4 p-2" />
-                <div className="grid grid-cols-2">
-                  <p className="">Name</p>
-                  <p className="">Time</p>
-                  <p className="col-span-2">Servings</p>
-                </div>
-              </div>
-              <div className="text-center border border-4 border-red-800 h-96 rounded-md">
-                <img src="" alt="" className="h-3/4 p-2" />
-                <div className="grid grid-cols-2">
-                  <p className="">Name</p>
-                  <p className="">Time</p>
-                  <p className="col-span-2">Servings</p>
-                </div>
-              </div>
-              <div className="text-center border border-4 border-red-800 h-96 rounded-md">
-                <img src="" alt="" className="h-3/4 p-2" />
-                <div className="grid grid-cols-2">
-                  <p className="">Name</p>
-                  <p className="">Time</p>
-                  <p className="col-span-2">Servings</p>
-                </div>
-              </div>
-              <div className="text-center border border-4 border-red-800 h-96 rounded-md">
-                <img src="" alt="" className="h-3/4 p-2" />
-                <div className="grid grid-cols-2">
-                  <p className="">Name</p>
-                  <p className="">Time</p>
-                  <p className="col-span-2">Servings</p>
-                </div>
-              </div>
-              <div className="text-center border border-4 border-red-800 h-96 rounded-md">
-                <img src="" alt="" className="h-3/4 p-2" />
-                <div className="grid grid-cols-2">
-                  <p className="">Name</p>
-                  <p className="">Time</p>
-                  <p className="col-span-2">Servings</p>
-                </div>
-              </div>
-              <div className="text-center border border-4 border-red-800 h-96 rounded-md">
-                <img src="" alt="" className="h-3/4 p-2" />
-                <div className="grid grid-cols-2">
-                  <p className="">Name</p>
-                  <p className="">Time</p>
-                  <p className="col-span-2">Servings</p>
-                </div>
-              </div>
-              <div className="text-center border border-4 border-red-800 h-96 rounded-md">
-                <img src="" alt="" className="h-3/4 p-2" />
-                <div className="grid grid-cols-2">
-                  <p className="">Name</p>
-                  <p className="">Time</p>
-                  <p className="col-span-2">Servings</p>
-                </div>
-              </div>
-              <div className="text-center border border-4 border-red-800 h-96 rounded-md">
-                <img src="" alt="" className="h-3/4 p-2" />
-                <div className="grid grid-cols-2">
-                  <p className="">Name</p>
-                  <p className="">Time</p>
-                  <p className="col-span-2">Servings</p>
-                </div>
-              </div>
-              <div className="text-center border border-4 border-red-800 h-96 rounded-md">
-                <img src="" alt="" className="h-3/4 p-2" />
-                <div className="grid grid-cols-2">
-                  <p className="">Name</p>
-                  <p className="">Time</p>
-                  <p className="col-span-2">Servings</p>
-                </div>
-              </div>
-              <div className="text-center border border-4 border-red-800 h-96 rounded-md">
-                <img src="" alt="" className="h-3/4 p-2" />
-                <div className="grid grid-cols-2">
-                  <p className="">Name</p>
-                  <p className="">Time</p>
-                  <p className="col-span-2">Servings</p>
-                </div>
-              </div>
-              <div className="text-center border border-4 border-red-800 h-96 rounded-md">
-                <img src="" alt="" className="h-3/4 p-2" />
-                <div className="grid grid-cols-2">
-                  <p className="">Name</p>
-                  <p className="">Time</p>
-                  <p className="col-span-2">Servings</p>
-                </div>
+              <div
+                className="relative w-64 h-64"
+                onMouseEnter={handleMouseEnter}
+                onMouseLeave={handleMouseLeave}
+                onTouchStart={handleTouchStart}
+                onTouchEnd={handleTouchEnd}
+              >
+                <img
+                  src="https://cdn.britannica.com/86/145786-050-5BD27317/chef-cooking-restaurant-kitchen.jpg"
+                  alt=""
+                  className="w-full h-full object-cover"
+                />
+
+                {isHovered && (
+                  <div className="absolute inset-0 flex items-center justify-center bg-black bg-opacity-50">
+                    <div className="text-white text-center">
+                      <h3 className="text-xl font-semibold mb-2">Recipe</h3>
+                      <p>description</p>
+                    </div>
+                  </div>
+                )}
               </div>
             </div>
           </main>

e94810faed8adef406fb133f7adede6af7b78052
irriz-jayy <jayfelix789@gmail.com>
2023-06-14 11:24:23 +0300

    added styking ti card

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -30,16 +30,8 @@ function Homepage() {
               All recipes
             </div>
             <div className="border border-4 border-red-800 m-4 p-2 grid grid-cols-3 gap-4">
-              <div className="text-center border border-4 border-red-800 h-96 rounded-md">
-                <img src="" alt="" className="h-3/4 p-2" />
-                <div className="grid grid-cols-2">
-                  <p className="">Name</p>
-                  <p className="">Time</p>
-                  <p className="col-span-2">Servings</p>
-                </div>
-              </div>
               <div
-                className="relative w-64 h-64"
+                className="relative h-96"
                 onMouseEnter={handleMouseEnter}
                 onMouseLeave={handleMouseLeave}
                 onTouchStart={handleTouchStart}
@@ -48,14 +40,18 @@ function Homepage() {
                 <img
                   src="https://cdn.britannica.com/86/145786-050-5BD27317/chef-cooking-restaurant-kitchen.jpg"
                   alt=""
-                  className="w-full h-full object-cover"
+                  className="w-full h-full object-cover "
                 />
 
                 {isHovered && (
                   <div className="absolute inset-0 flex items-center justify-center bg-black bg-opacity-50">
-                    <div className="text-white text-center">
-                      <h3 className="text-xl font-semibold mb-2">Recipe</h3>
+                    <div className="text-white text-center font-curve">
+                      <h3 className="text-xl font-semibold mb-2">
+                        Recipe name
+                      </h3>
                       <p>description</p>
+                      <p>time</p>
+                      <p>servings</p>
                     </div>
                   </div>
                 )}

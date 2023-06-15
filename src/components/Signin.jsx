54c216b60143f3025b222b1af5e598932322f1d3
irriz-jayy <jayfelix789@gmail.com>
2023-06-15 20:28:49 +0300

    added background image and overlay color

--- src/components/Signin.jsx
+++ src/components/Signin.jsx
@@ -2,11 +2,14 @@ import React from "react";
 
 function Signin() {
   return (
-    <>
-      <div className="h-[100vh]">
-        <img src="" alt="background"></img>
-      </div>
-    </>
+    <div className="h-screen flex items-center justify-center">
+      <div className="absolute inset-0 bg-background1 bg-opacity-30 mix-blend-multiply"></div>
+      <img
+        src="https://assets.bonappetit.com/photos/5e7a6c79edf206000862e452/16:9/w_2580,c_limit/Cooking-Home-Collection.jpg"
+        alt="background"
+        className="h-screen w-screen mix-blend-screen"
+      />
+    </div>
   );
 }
 

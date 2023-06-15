f72e0521d0501809c978c9e63686a8738bc0ddc2
irriz-jayy <jayfelix789@gmail.com>
2023-06-15 19:57:36 +0300

    created sign in page

---
+++ src/components/Signin.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Signin() {
+  return (
+    <>
+      <div className="h-[100vh]">
+        <img src="" alt="background"></img>
+      </div>
+    </>
+  );
+}
+
+export default Signin;

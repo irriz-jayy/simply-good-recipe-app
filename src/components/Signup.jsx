4a4cadbf2119ee6388a492ad4ced707247fcef30
irriz-jayy <jayfelix789@gmail.com>
2023-06-16 10:24:35 +0300

    completed initial sign up form

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -13,11 +13,11 @@ function Signin() {
       <div className="h-screen flex items-center justify-center">
         <div className="bg-background1 bg-opacity-80 p-6 rounded-lg shadow-lg relative z-10">
           <form>
-            <p className="text-center text-3xl font-curve">Sign in</p>
+            <p className="text-center text-3xl font-curve">Sign up</p>
             <p className="text-center font-curve">
-              Don't have an account?{" "}
-              <a href="/sign-up" className="text-purple-800">
-                Sign up
+              Already have an account?{" "}
+              <a href="/sign-in" className="text-purple-800">
+                Log in
               </a>
             </p>
             <label htmlFor="email" className="block mb-2 font-curve">
@@ -26,6 +26,22 @@ function Signin() {
             <input
               type="text"
               id="email"
+              className="w-full px-3 py-2 mb-4 border rounded-md font-curve font-light"
+            />
+            <label htmlFor="name" className="block mb-2 font-curve">
+              Full Name
+            </label>
+            <input
+              type="text"
+              id="name"
+              className="w-full px-3 py-2 mb-4 border rounded-md font-curve font-light"
+            />
+            <label htmlFor="profile-picture" className="block mb-2 font-curve">
+              Profile Picture
+            </label>
+            <input
+              type="file"
+              id="pfp"
               className="w-full px-3 py-2 mb-4 border rounded-md font-curve font-light"
             />
             <label htmlFor="password" className="block mb-2 font-curve">
@@ -40,7 +56,7 @@ function Signin() {
               type="submit"
               className="w-full px-4 py-2 bg-background1 border font-curve hover:bg-blue-800 hover:bg-opacity-30 text-font1 rounded"
             >
-              Sign In
+              Sign up
             </button>
           </form>
         </div>

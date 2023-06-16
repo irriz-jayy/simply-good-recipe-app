b6ce1d8ad9f724c2eb089a21aea1a3ce7bcb4d31
irriz-jayy <jayfelix789@gmail.com>
2023-06-16 11:58:57 +0300

    added boilerplate code for the card

--- src/components/RecipeDetails.jsx
+++ src/components/RecipeDetails.jsx
@@ -1,7 +1,25 @@
 import React from "react";
+import Sidebar from "./Sidebar";
 
 function RecipeDetails() {
-  return <div>RecipeDetails</div>;
+  return (
+    <>
+      <div className="flex">
+        <Sidebar />
+        <div className="flex-1 bg-background2 bg-opacity-30">
+          <main className="min-h-screen flex items-center justify-center">
+            <div className="w-5/6 bg-white border border-red-800 m-4">
+              <div className="h-48 w-full bg-red-800">image and name</div>
+              <div className="h-96 w-full bg-yellow-400">Details</div>
+              <div className="h-96 w-full bg-yellow-600">
+                Cooking instructions
+              </div>
+            </div>
+          </main>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default RecipeDetails;

ffb33b42df58eecdbe3a8212b6a3cdb198c5d560
irriz-jayy <jayfelix789@gmail.com>
2023-06-16 13:05:18 +0300

    finifhed initial card look

--- src/components/RecipeDetails.jsx
+++ src/components/RecipeDetails.jsx
@@ -8,7 +8,7 @@ function RecipeDetails() {
         <Sidebar />
         <div className="flex-1 bg-background2 bg-opacity-30">
           <main className="min-h-screen flex items-center justify-center">
-            <div className="w-5/6  m-4 bg-blue-800  bg-opacity-40 shadow-lg rounded-md">
+            <div className="w-5/6  m-4 bg-white shadow-2xl rounded-md">
               <div className="h-60 w-full grid grid-cols-2">
                 <img
                   src="https://cdn.britannica.com/86/145786-050-5BD27317/chef-cooking-restaurant-kitchen.jpg"
@@ -27,9 +27,35 @@ function RecipeDetails() {
                   </p>
                 </div>
               </div>
-              <div className="h-96 w-full bg-yellow-400"></div>
-              <div className="h-96 w-full bg-yellow-600">
-                Cooking instructions
+              <div className="h-96 w-full ">
+                <p className="text-center text-2xl font-curve font-medium p-2">
+                  Ingredients
+                </p>
+                <ul className="ml-8 font-serif text-lg font-light list-decimal">
+                  <li>Ingredient 1</li>
+                  <li>Ingredient 1</li>
+                  <li>Ingredient 1</li>
+                  <li>Ingredient 1</li>
+                  <li>Ingredient 1</li>
+                  <li>Ingredient 1</li>
+                  <li>Ingredient 1</li>
+                  <li>Ingredient 1</li>
+                </ul>
+              </div>
+              <div className="h-96 w-full">
+                <p className="text-center text-2xl font-curve font-medium p-2">
+                  Cooking instructions
+                </p>
+                <ul className="ml-8 font-serif text-lg font-light list-disc">
+                  <li>Step 1</li>
+                  <li>Step 1</li>
+                  <li>Step 1</li>
+                  <li>Step 1</li>
+                  <li>Step 1</li>
+                  <li>Step 1</li>
+                  <li>Step 1</li>
+                  <li>Step 1</li>
+                </ul>
               </div>
             </div>
           </main>

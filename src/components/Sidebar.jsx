0cf058342405b98d70cb7dcdbbda2c79ef724877
irriz-jayy <jayfelix789@gmail.com>
2023-06-17 13:18:31 +0300

    minor changes

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -166,7 +166,7 @@ export default function Example() {
           </div>
         </div>
         <div className="flex flex-1 flex-col md:pl-64">
-          <div className="sticky top-0 z-10 bg-background2 bg-opacity-30 h-[100vh] pl-1 pt-1 sm:pl-3 sm:pt-3 md:hidden">
+          <div className="sticky top-0 z-10 bg-background2 bg-opacity-30 min-h-[100vh] pl-1 pt-1 sm:pl-3 sm:pt-3 md:hidden">
             <button
               type="button"
               className="-ml-0.5 -mt-0.5 inline-flex h-12 w-12 items-center justify-center rounded-md text-gray-500 hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500"

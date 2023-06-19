69240f76cb6b41e12bc139a245859013588e5730
irriz-jayy <jayfelix789@gmail.com>
2023-06-19 21:19:44 +0300

    added carousel

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,7 +1,50 @@
 import React from "react";
+import "../App.css";
+import { Navigation, Pagination, Scrollbar, A11y } from "swiper";
 
-function Carousel() {
-  return <div className="text-center">Carousel</div>;
-}
+import { Swiper, SwiperSlide } from "swiper/react";
 
-export default Carousel;
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "swiper/css/scrollbar";
+
+export default () => {
+  return (
+    <>
+      <div className="h-96">
+        <Swiper
+          slidesPerView={"auto"}
+          spaceBetween={30}
+          navigation={true}
+          loop={true}
+          pagination={{
+            clickable: true,
+          }}
+          modules={[Pagination]}
+          className="mySwiper"
+        >
+          <SwiperSlide>
+            <img src="https://bit.ly/3MSUbSo" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src="https://bit.ly/3oUyjxY" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src="https://bit.ly/3OIiOUD" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src="https://bit.ly/43IRU34" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src="https://bit.ly/45REbbX" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src="https://bit.ly/3IYIGYB" />
+          </SwiperSlide>
+        </Swiper>{" "}
+      </div>
+    </>
+  );
+};

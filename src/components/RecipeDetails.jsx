6b360be819fd9dea9fad538dad975b8fc5114e7b
irriz-jayy <jayfelix789@gmail.com>
2023-06-19 12:36:07 +0300

    changed fonts to the recipe detail card

--- src/components/RecipeDetails.jsx
+++ src/components/RecipeDetails.jsx
@@ -16,22 +16,22 @@ function RecipeDetails() {
                   className="h-56 w-3/4 m-2 rounded-md "
                 />
                 <div className="h-56 m-2 rounded-md">
-                  <p className="text-center text-2xl font-curve font-medium p-2">
+                  <p className="text-center text-2xl font-heading font-medium p-2">
                     Title
                   </p>
-                  <p className="text-center text-lg font-curve font-medium p-2">
+                  <p className="text-center text-lg font-heading font-medium p-2">
                     Country
                   </p>
-                  <p className="text-center text-lg font-curve font-medium p-2">
+                  <p className="text-center text-lg font-heading font-medium p-2">
                     Rating
                   </p>
                 </div>
               </div>
               <div className="h-96 w-full ">
-                <p className="text-center text-2xl font-curve font-medium p-2">
+                <p className="text-center text-2xl font-heading font-medium p-2">
                   Ingredients
                 </p>
-                <ul className="ml-8 font-serif text-lg font-light list-decimal">
+                <ul className="ml-8 font-serif text-lg font-light font-paragraph list-decimal">
                   <li>Ingredient 1</li>
                   <li>Ingredient 1</li>
                   <li>Ingredient 1</li>
@@ -43,10 +43,10 @@ function RecipeDetails() {
                 </ul>
               </div>
               <div className="h-96 w-full">
-                <p className="text-center text-2xl font-curve font-medium p-2">
+                <p className="text-center text-2xl font-heading font-medium p-2">
                   Cooking instructions
                 </p>
-                <ul className="ml-8 font-serif text-lg font-light list-disc">
+                <ul className="ml-8 font-serif text-lg font-light font-paragraph list-disc">
                   <li>Step 1</li>
                   <li>Step 1</li>
                   <li>Step 1</li>

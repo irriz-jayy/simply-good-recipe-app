70fb09adf3886a830d29dd4220b184da4a06afba
irriz-jayy <jayfelix789@gmail.com>
2023-07-15 12:18:04 +0300

    rendered recipes

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -7,14 +7,15 @@ import Carousel from "./Carousel";
 import { fetchRecipes } from "../api/recipe";
 
 function Homepage() {
+  const [recipes, setRecipes] = useState([]);
+
   useEffect(() => {
     const fetchRecipesData = async () => {
       try {
-        const recipes = await fetchRecipes();
-        console.log("Fetched recipes:", recipes);
-        // Set the fetched recipes in state or perform any other logic
+        const recipesData = await fetchRecipes();
+        console.log("Fetched recipes:", recipesData);
+        setRecipes(recipesData);
       } catch (error) {
-        // Handle error
         console.error("Error fetching recipes:", error);
       }
     };
@@ -61,182 +62,43 @@ function Homepage() {
             <div className="text-left font-medium font-curve text-2xl flex items-center text-font2 ml-4 h-12">
               Just for you
             </div>
+            {/* recipe card model */}
             <div className="m-4 p-2 grid grid-cols-1 gap-4 flex items-center justify-center md:grid md:grid-cols-2 md:gap-2 lg:grid lg:grid-cols-3">
-              <div
-                className="relative"
-                onMouseEnter={handleMouseEnter}
-                onMouseLeave={handleMouseLeave}
-                onTouchStart={handleTouchStart}
-                onTouchEnd={handleTouchEnd}
-              >
-                <img
-                  src="https://cdn.britannica.com/86/145786-050-5BD27317/chef-cooking-restaurant-kitchen.jpg"
-                  alt=""
-                  className="w-full h-full object-cover "
-                />
+              {recipes.map((recipe) => (
+                <div
+                  key={recipe.id}
+                  className="relative"
+                  onMouseEnter={handleMouseEnter}
+                  onMouseLeave={handleMouseLeave}
+                  onTouchStart={handleTouchStart}
+                  onTouchEnd={handleTouchEnd}
+                >
+                  <img
+                    src={recipe.image_url}
+                    alt=""
+                    className="w-full h-full object-cover"
+                  />
 
-                {isHovered && (
-                  <div className="absolute inset-0 flex items-center justify-center bg-black bg-opacity-50 hover:cursor-pointer">
-                    <div className="text-white text-center font-curve">
-                      <h3 className="text-xl font-semibold mb-2">
-                        Recipe name
-                      </h3>
-                      <p>description</p>
-                      <p>time</p>
-                      <p>servings</p>
-                      <div
-                        className="absolute bottom-6 left-1/2 transform -translate-x-1/2 hover:cursor-pointer"
-                        // onClick={handleFavourite}
-                      >
-                        <StarIcon
-                          className="h-6 w-6 text-white"
-                          aria-hidden="true"
-                        />
+                  {isHovered && (
+                    <div className="absolute inset-0 flex items-center justify-center bg-black bg-opacity-50 hover:cursor-pointer">
+                      <div className="text-white text-center font-curve">
+                        <h3 className="text-xl font-semibold mb-2">
+                          {recipe.name}
+                        </h3>
+                        <p>{recipe.description}</p>
+                        <p>Time: {recipe.time}</p>
+                        <p>Servings: {recipe.number_of_people_served}</p>
+                        <div className="absolute bottom-6 left-1/2 transform -translate-x-1/2 hover:cursor-pointer">
+                          <StarIcon
+                            className="h-6 w-6 text-white"
+                            aria-hidden="true"
+                          />
+                        </div>
                       </div>
                     </div>
-                  </div>
-                )}
-              </div>
-              <div
-                className="relative"
-                onMouseEnter={handleMouseEnter}
-                onMouseLeave={handleMouseLeave}
-                onTouchStart={handleTouchStart}
-                onTouchEnd={handleTouchEnd}
-              >
-                <img
-                  src="https://cdn.britannica.com/86/145786-050-5BD27317/chef-cooking-restaurant-kitchen.jpg"
-                  alt=""
-                  className="w-full h-full object-cover "
-                />
-
-                {isHovered && (
-                  <div className="absolute inset-0 flex items-center justify-center bg-black bg-opacity-50 hover:cursor-pointer">
-                    <div className="text-white text-center font-curve">
-                      <h3 className="text-xl font-semibold mb-2">
-                        Recipe name
-                      </h3>
-                      <p>description</p>
-                      <p>time</p>
-                      <p>servings</p>
-                      <div
-                        className="absolute bottom-6 left-1/2 transform -translate-x-1/2 hover:cursor-pointer"
-                        // onClick={handleFavourite}
-                      >
-                        <StarIcon
-                          className="h-6 w-6 text-white"
-                          aria-hidden="true"
-                        />
-                      </div>
-                    </div>
-                  </div>
-                )}
-              </div>
-              <div
-                className="relative"
-                onMouseEnter={handleMouseEnter}
-                onMouseLeave={handleMouseLeave}
-                onTouchStart={handleTouchStart}
-                onTouchEnd={handleTouchEnd}
-              >
-                <img
-                  src="https://cdn.britannica.com/86/145786-050-5BD27317/chef-cooking-restaurant-kitchen.jpg"
-                  alt=""
-                  className="w-full h-full object-cover "
-                />
-
-                {isHovered && (
-                  <div className="absolute inset-0 flex items-center justify-center bg-black bg-opacity-50 hover:cursor-pointer">
-                    <div className="text-white text-center font-curve">
-                      <h3 className="text-xl font-semibold mb-2">
-                        Recipe name
-                      </h3>
-                      <p>description</p>
-                      <p>time</p>
-                      <p>servings</p>
-                      <div
-                        className="absolute bottom-6 left-1/2 transform -translate-x-1/2 hover:cursor-pointer"
-                        // onClick={handleFavourite}
-                      >
-                        <StarIcon
-                          className="h-6 w-6 text-white"
-                          aria-hidden="true"
-                        />
-                      </div>
-                    </div>
-                  </div>
-                )}
-              </div>
-              <div
-                className="relative"
-                onMouseEnter={handleMouseEnter}
-                onMouseLeave={handleMouseLeave}
-                onTouchStart={handleTouchStart}
-                onTouchEnd={handleTouchEnd}
-              >
-                <img
-                  src="https://cdn.britannica.com/86/145786-050-5BD27317/chef-cooking-restaurant-kitchen.jpg"
-                  alt=""
-                  className="w-full h-full object-cover "
-                />
-
-                {isHovered && (
-                  <div className="absolute inset-0 flex items-center justify-center bg-black bg-opacity-50 hover:cursor-pointer">
-                    <div className="text-white text-center font-curve">
-                      <h3 className="text-xl font-semibold mb-2">
-                        Recipe name
-                      </h3>
-                      <p>description</p>
-                      <p>time</p>
-                      <p>servings</p>
-                      <div
-                        className="absolute bottom-6 left-1/2 transform -translate-x-1/2 hover:cursor-pointer"
-                        // onClick={handleFavourite}
-                      >
-                        <StarIcon
-                          className="h-6 w-6 text-white"
-                          aria-hidden="true"
-                        />
-                      </div>
-                    </div>
-                  </div>
-                )}
-              </div>
-              <div
-                className="relative"
-                onMouseEnter={handleMouseEnter}
-                onMouseLeave={handleMouseLeave}
-                onTouchStart={handleTouchStart}
-                onTouchEnd={handleTouchEnd}
-              >
-                <img
-                  src="https://cdn.britannica.com/86/145786-050-5BD27317/chef-cooking-restaurant-kitchen.jpg"
-                  alt=""
-                  className="w-full h-full object-cover "
-                />
-
-                {isHovered && (
-                  <div className="absolute inset-0 flex items-center justify-center bg-black bg-opacity-50 hover:cursor-pointer">
-                    <div className="text-white text-center font-curve">
-                      <h3 className="text-xl font-semibold mb-2">
-                        Recipe name
-                      </h3>
-                      <p>description</p>
-                      <p>time</p>
-                      <p>servings</p>
-                      <div
-                        className="absolute bottom-6 left-1/2 transform -translate-x-1/2 hover:cursor-pointer"
-                        // onClick={handleFavourite}
-                      >
-                        <StarIcon
-                          className="h-6 w-6 text-white"
-                          aria-hidden="true"
-                        />
-                      </div>
-                    </div>
-                  </div>
-                )}
-              </div>
+                  )}
+                </div>
+              ))}
             </div>
           </main>
         </div>
